a0618d6178df6c7fc93f5b1b79037431fd0794f0
undacat <shannon@ruder.net>
2019-02-05 00:12:57 -0800

    yoda image changes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,7 +4,7 @@ var path = "./assets/images/";
 
 var categories = ["inspire", "management", "sports", "life", "funny", "love", "art", "students"];
 
-var yodaImages = ["yoda1.jpeg","yoda2.jpeg", "yoda3.jpeg", "yoda4.jpeg", "yoda5.jpeg", "yoda6.jpeg", "yoda7.jpeg"];// ****** TASK Create array of photos, find photos to use in case quote api does not provide us with a background
+var yodaImages = ["yoda1.jpeg","yoda2.jpeg", "yoda3.jpeg", "yoda4.jpeg", "yoda5.jpeg", "yoda6.jpeg"];// ****** TASK Create array of photos, find photos to use in case quote api does not provide us with a background
 
 var randomNum = Math.floor(Math.random() * yodaImages.length);
 console.log(randomNum);
